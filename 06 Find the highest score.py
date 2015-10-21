037c00b55ea9d0158b6d39d8679f61c7b66f3be8
CapitanShinChan <capitan.shinchan@gmail.com>
2015-10-22 00:33:50 +0200

    Update 6th challenge solved

--- 06 Find the highest score.py
+++ 06 Find the highest score.py
@@ -4,4 +4,12 @@ import sys
 
 with open(sys.argv[1], 'r') as f:
     for line in f.readlines():
-        
+        groups = line.replace('\n','').split(' | ')
+        g_len = len(groups)
+        highs = groups[0].split(' ')
+        for group in groups[1:]:
+            numbers = group.split(' ')
+            for i in xrange(0, len(numbers)):
+                if int(numbers[i]) > int(highs[i]):
+                    highs[i] = numbers[i]
+        print ' '.join(highs)
